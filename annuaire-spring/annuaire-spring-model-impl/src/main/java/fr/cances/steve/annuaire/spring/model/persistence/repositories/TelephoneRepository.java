3a025f26a6ab3ab433210998a81929063afc8983
Steve Cancès <steve.cances@gmail.com>
2014-04-13 18:23:26 +0200

    Ajout du dao TelephoneRepository et de TU ServiceAnnuaireAdminImplTest

---
+++ annuaire-spring/annuaire-spring-model-impl/src/main/java/fr/cances/steve/annuaire/spring/model/persistence/repositories/TelephoneRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fr.cances.steve.annuaire.spring.model.persistence.repositories;
+
+import fr.cances.steve.annuaire.spring.model.persistence.entities.Telephone;
+
+/**
+ * @author Steve Cancès
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ */
+public interface TelephoneRepository extends AbstractRepository<Telephone, Long> {
+
+}
--- annuaire-spring/annuaire-spring-model-impl/src/main/java/fr/cances/steve/annuaire/spring/model/persistence/repositories/jpa/PersonneRepositoryJpa.java
+++ annuaire-spring/annuaire-spring-model-impl/src/main/java/fr/cances/steve/annuaire/spring/model/persistence/repositories/jpa/PersonneRepositoryJpa.java
@@ -9,13 +9,30 @@ import org.springframework.stereotype.Repository;
 import fr.cances.steve.annuaire.spring.model.persistence.entities.Personne;
 import fr.cances.steve.annuaire.spring.model.persistence.repositories.PersonneRepository;
 
+/**
+ * @author Steve Cancès
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ */
 @Repository
 public class PersonneRepositoryJpa extends AbstractRepositoryJpa<Personne, Long> implements PersonneRepository {
 
+	/**
+	 * @author Steve Cancès
+	 * @Since 1.0.0
+	 * 
+	 * @param entityClass
+	 */
 	public PersonneRepositoryJpa(Class<Personne> entityClass) {
 		super(entityClass);
 	}
 
+	/**
+	 * @author Steve Cancès
+	 * @Since 1.0.0
+	 *
+	 */
 	public PersonneRepositoryJpa() {
 		this(Personne.class);
 	}
