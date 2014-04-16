aa55e43b6857a0a6e43820706c97577ac99d2f3f
Steve Cancès <steve.cances@gmail.com>
2014-04-16 22:30:02 +0200

    Gestion de l'internationalisation

--- annuaire-spring/annuaire-spring-ws/src/main/java/fr/cances/steve/annuaire/spring/ws/config/WebMvcConfig.java
+++ annuaire-spring/annuaire-spring-ws/src/main/java/fr/cances/steve/annuaire/spring/ws/config/WebMvcConfig.java
@@ -1,20 +1,49 @@
 package fr.cances.steve.annuaire.spring.ws.config;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	private static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
 	
+	private static final String LOCALE_CHANGE_INTERCEPTOR_PARAM_NAME = "lang";
+
 	@Bean
-	public ResourceBundleMessageSource messageSource() {
+	public ResourceBundleMessageSource  messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("messages/messages");
 		messageSource.setUseCodeAsDefaultMessage(true);
 		return messageSource;
+	}
+
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(DEFAULT_LOCALE);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName(LOCALE_CHANGE_INTERCEPTOR_PARAM_NAME);
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 
 }
